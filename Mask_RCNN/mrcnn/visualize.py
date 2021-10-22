37278314f6711472ecad416394c5f7e82b5a8505
imaruri <im818@ic.ac.uk>
2021-10-22 17:45:30 +0200

    Dependencies fully updated

--- Mask_RCNN/mrcnn/visualize.py
+++ Mask_RCNN/mrcnn/visualize.py
@@ -165,7 +165,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
-        plt.savefig("/Users/mym/Desktop/kaluli/static/filess.jpg")
+        plt.savefig("output.png")
         #plt.show()
 
 
